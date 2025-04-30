8afbdb5f554371f98e252c90dbe2cff9ae9cf02a
Nehaka-del <neha.a1@s.amity.edu>
2025-04-30 20:49:24 +0530

    created a c file for finding maximum and minimum in array

---
+++ new2.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main() {
+    int numbers[]={10,20,35,40,50,75,60};
+    int size=sizeof(numbers)/sizeof(numbers[0]);
+
+    int max=numbers[0];
+
+    for(int i=1;i<size;i++){
+        if(numbers[i]>max){
+            max=numbers[i];
+        }
+    }
+
+    int min=numbers[0];
+
+    for(int i=1;i<size;i++) {
+        if(numbers[i]<min) {
+
+        }
+    }
+
+    printf("The largest number is:%d\n",max);
+    printf("The largest number is:%d\n",min);
+    return 0;
+}
+
